e4aa0422a73f0e0d1913a31816c4835a44778ed2
darkavengerk <darkavengerk@gmail.com>
2016-01-17 13:14:24 +0900

    test ssh

--- server/word-parser.py
+++ server/word-parser.py
@@ -25,6 +25,7 @@ def tokenize(source):
 			made.append(word)
 	return made
 
+
 def milliseconds(time_object):
 	return (((time_object.hours * 60) + time_object.minutes) * 60 + time_object.seconds) * 1000 + time_object.milliseconds;
 
